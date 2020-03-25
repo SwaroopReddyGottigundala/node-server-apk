6124b9f7526eebfa6feea942efcb5c4c28d3c2f5
swaroop reddy <reddy.swaroop2016@gmail.com>
2020-03-24 23:03:22 -0500

    ajdc

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require('http')  // require built-in Node.js http package
 
 // Use hosting values if available, otherwise default 
 const hostname = process.env.hostname || '0.0.0.0' // allow remote access
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3001
 
 // define our server
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World! This is text - Hola Mundo :)\n')
+  res.end('Hello World! This is text - we can respond with HTML, JSON, and more :)\n')
 })
 
 // start listening
